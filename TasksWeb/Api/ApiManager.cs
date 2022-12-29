d3a78b5438a2bf2636338029f2aa3386bd800fa6
Standa Procházka <prochst@google.com>
2022-12-29 10:46:25 +0100

    Finish comments

--- TasksWeb/Api/ApiManager.cs
+++ TasksWeb/Api/ApiManager.cs
@@ -18,20 +18,19 @@ namespace TasksWeb.Api
             Delete
         }
 
-        static string ApiBaseUri = "http://localhost:5050/";
-        static HttpClient client = new HttpClient { BaseAddress = new Uri(ApiManager.ApiBaseUri) };
+        static readonly string ApiBaseUri = "http://localhost:5050/";
+        static readonly HttpClient client = new() { BaseAddress = new Uri(ApiManager.ApiBaseUri) };
 
         /// <summary>
         /// General method for call api action
         /// </summary>
-        /// <param name="apiAction">Action fro enum ApiAction</param>
+        /// <param name="apiAction">Action from enum ApiAction</param>
         /// <param name="uri">URI string for API action</param>
         /// <param name="data">data object for PUT or POST action</param>
-        /// <returns>Response of action as JSON string</returns>
-        public static async Task<String> SendApiRequest(ApiAction apiAction, string uri, Object data = null)
+        /// <returns>Response of action as JSON string or null</returns>
+        public static async Task<String> SendApiRequest(ApiAction apiAction, string uri, Object data = null!)
         {
-            //StringContent content;
-            HttpResponseMessage responseMessage = new HttpResponseMessage();
+            HttpResponseMessage responseMessage = new();
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
@@ -66,7 +65,7 @@ namespace TasksWeb.Api
         /// <summary>
         /// Gets Tasks
         /// </summary>
-        /// <param name="icludedDeleted">if true - return task also task with state.deleted</param>
+        /// <param name="icludedDeleted">if true - return also task with state.deleted</param>
         /// <returns>Response of action as JSON string</returns>
         public static async Task<List<MyTask>?> GetTasks(Boolean icludedDeleted = false)
         {
@@ -81,7 +80,7 @@ namespace TasksWeb.Api
         /// </summary>
         /// <param name="username"></param>
         /// <param name="password"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>User as JSON string or null</returns>
         public static async Task<User?> VerifyUser(string username, string password)
         {
             string uri = "users/" + username + "/" + password;
@@ -93,10 +92,10 @@ namespace TasksWeb.Api
         /// </summary>
         /// <param name="username"></param>
         /// <param name="password"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>New user as JSON string or null</returns>
         public static async Task<User?> RegisterUser(string username, string password)
         {
-            User newUser = new User { userName = username, password = password };
+            User newUser = new() { userName = username, password = password };
             string uri = "users";
             var responseContent = await SendApiRequest(ApiAction.Post, uri, newUser);
             return JsonSerializer.Deserialize<User>(responseContent);
@@ -105,7 +104,7 @@ namespace TasksWeb.Api
         /// Update selected task
         /// </summary>
         /// <param name="myTask"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>Task as JSON string or null</returns>
         public static async Task<MyTask>? UpdateTask(MyTask myTask)
         {
             string uri = "tasks/" + myTask.id;
@@ -116,7 +115,7 @@ namespace TasksWeb.Api
         /// Create new task
         /// </summary>
         /// <param name="myTask"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>New Task as JSON string or null</returns>
         public static async Task<MyTask>? CreateTask(MyTask myTask)
         {
             string uri = "tasks";
@@ -127,7 +126,7 @@ namespace TasksWeb.Api
         /// Delete selected task and associated comments
         /// </summary>
         /// <param name="myTask"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>Deleted Task as JSON string or null</returns>
         public static async Task<MyTask>? DeleteTask(MyTask myTask, List<Comment> comments)
         {
             string uri = "tasks/" + myTask.id;
@@ -143,7 +142,7 @@ namespace TasksWeb.Api
         /// Gets all comments joined with selected task
         /// </summary>
         /// <param name="taskId"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>List<Comment> as JSON string or null</returns>
         public static async Task<List<Comment>?> GetComments4Taks(int taskId)
         {
             string uri = "comments/" + taskId;
@@ -154,7 +153,7 @@ namespace TasksWeb.Api
         /// Create new comment
         /// </summary>
         /// <param name="comment"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>New Comment as JSON string or null</returns>
         public static async Task<Comment>? CreateComment(Comment comment)
         {
             string uri = "comments";
@@ -165,23 +164,23 @@ namespace TasksWeb.Api
         /// Update comment
         /// </summary>
         /// <param name="comment"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>Comment as JSON string or null</returns>
         public static async Task<Comment>? UpdateComment(Comment comment)
         {
             string uri = "comments/" + comment.id; ;
             var responseContent = await SendApiRequest(ApiAction.Put, uri, comment);
-            return JsonSerializer.Deserialize<Comment>(responseContent);
+            return JsonSerializer.Deserialize<Comment>(responseContent)!;
         }
         /// <summary>
         /// Delete selected comment
         /// </summary>
         /// <param name="comment"></param>
-        /// <returns>Response of action as JSON string</returns>
+        /// <returns>Deleted Comment as JSON string or null</returns>
         public static async Task<Comment>? DeleteComment(Comment comment)
         {
             string uri = "comments/" + comment.id;
             var responseContent = await SendApiRequest(ApiAction.Delete, uri);
-            return JsonSerializer.Deserialize<Comment>(responseContent);
+            return JsonSerializer.Deserialize<Comment>(responseContent)!;
         }
     }
 }
